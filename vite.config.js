eb4d26a1cae9aa88b42d913070b66f984df2c0f6
azern32 <chiro.emiya@gmail.com>
2023-04-13 14:29:13 +0800

    pake build sendiri

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/azern32.github.io/",
+  // base: "/azern32.github.io/",
   plugins: [svelte()],
+  build: {
+    outDir: './docs', // relative to index.html
+    // emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
+  }
 })
