ddccd7c1f698a19685b7a06bfe1523345fb5e2d4
azern32 <chiro.emiya@gmail.com>
2023-04-14 10:40:10 +0800

    thumbnail

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,35 @@
 <script>
   import "./app.css";
   import Navbar from "./lib/Navbar.svelte";
+  import Projects from "./lib/Projects.svelte";
 </script>
 
 <main>
   <Navbar/>
+  
+  <!-- Projects List Section -->
+  <div class="flex flex-wrap justify-center">
+    <Projects 
+      gameName="Tembak-tembak" 
+      idTag="1stwah" 
+      imageLink="https://cdn.donmai.us/original/0c/60/__hatsune_miku_kasane_teto_and_kasane_teto_vocaloid_and_2_more_drawn_by_konohoshi__0c60c5c01df2f7dce21ef967c2ef6a08.png"
+    />
+    <Projects 
+      gameName="Lari-lari"
+      idTag="2ndwah"
+      imageLink="https://cdn.donmai.us/original/a6/18/__hatsune_miku_vocaloid_drawn_by_sofra__a6185943790b04e39889bf297207abed.jpg"
+    />
+  </div>
 
+  <!-- About Me -->
+  <div>
+
+  </div>
+
+  <!-- My Contact -->
+  <div>
+
+  </div>
 </main>
 
 <style>
---
+++ src/lib/Projects.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let idTag
+    export let imageLink = ""
+    export let gameName = "Game Name"
+</script>
+
+<div id={idTag} class="drop-shadow-md p-5">
+    <!-- Thumbnail -->
+    <div class="blur-[2px] hover:blur-none">
+        <img class="rounded-lg object-cover h-32 w-56" src="{imageLink}" alt="">
+    </div>
+
+    <!-- Title -->
+    <h4 class="text-lg font-semibold pt-2">
+        {gameName}
+    </h4>
+</div>
