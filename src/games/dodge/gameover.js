814d71b405dadff1c9ff66bb6f282290bd1f76fc
azern32 <chiro.emiya@gmail.com>
2023-05-24 16:40:06 +0800

    pindah gambar

--- src/games/dodge/gameover.js
+++ src/games/dodge/gameover.js
@@ -1,7 +1,7 @@
 import { Sprite } from "pixi.js";
 
 
-const gameover = Sprite.from('src/assets/images/gameover.png')
+const gameover = Sprite.from('/gameover.png')
 gameover.anchor.set(.5)
 gameover.x = window.innerWidth / 2
 gameover.y = (window.innerHeight / 2 ) - 100
