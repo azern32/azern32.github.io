3507aa17376468d89e8ce1dfcb72c7916a1dfb3b
azern32 <chiro.emiya@gmail.com>
2023-08-07 17:30:09 +0800

    nda tau apa

--- src/components/content/Hero.svelte
+++ src/components/content/Hero.svelte
@@ -1,3 +1,13 @@
-<div class="w-full">
-	<img src="/hero.jpg" alt="" class="object-cover" />
+<div class="hero bg-cover" style="background-image: url(/hero.jpg);">
+	<div class="hero-overlay bg-opacity-60" />
+	<div class="hero-content text-center text-neutral-content">
+		<div class="max-w-md">
+			<h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+			<p class="mb-5">
+				Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+				quasi. In deleniti eaque aut repudiandae et a id nisi.
+			</p>
+			<button class="btn btn-primary">Get Started</button>
+		</div>
+	</div>
 </div>
