10d1b23d977d58ea20d9ef86afa23dec310d7764
azern32 <chiro.emiya@gmail.com>
2023-08-08 17:57:10 +0800

    shits

--- src/components/content/LovedStacks.svelte
+++ src/components/content/LovedStacks.svelte
@@ -1,1 +1,16 @@
-<div class="card">saya</div>
+<div class="card card-side p-4">
+	<div class="card-header bg-slate-500">
+		<div class="card-title -rotate-90 bottom-0">judul</div>
+	</div>
+	<div class="card-body">
+		<p>here some bullshits</p>
+		<p>
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas impedit deserunt voluptate
+			nostrum hic consequatur a sequi qui. Vero deleniti fuga culpa, aliquam impedit aspernatur sint
+			aperiam quaerat fugit voluptate!
+		</p>
+	</div>
+	<figure class="min-w-[12rem]">
+		<img class="object-contain w-80" src="/hero.jpg" alt="side" />
+	</figure>
+</div>
